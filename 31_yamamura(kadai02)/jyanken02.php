dd3d28d21432835e80022df6f6210f55378d7135
cat5cat5cat <cat0melody@gmail.com>
2019-02-24 21:52:10 +0900

    第二回の課題の再提出です。

---
+++ 31_yamamura(kadai02)/jyanken02.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+	<title> janken.php </title>
+	<link rel="stylesheet" href="css_main.css" type="text/css" media="all" />
+</head>
+<body>
+	<h1>janken.php</h1>
+	<div class="main">
+		<form id="janken" method="GET" action="janken02-2.php">
+		<h2>じゃんけん？</h2>
+		<div>
+			<select name="janken">
+				<option value="goo">グー！</option>
+				<option value="choki">チョキ！</option>
+				<option value="paa">パー！</option>
+			</select>
+			<br />
+			<br />
+			<input type="submit" value="勝負！" />
+		</div>
+		</form>
+	</div>
+</body>
+</html>
