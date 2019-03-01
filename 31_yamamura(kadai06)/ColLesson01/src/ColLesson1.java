ec1e58ae3eec06b19393fe5737f31abaf44c9664
cat5cat5cat <cat0melody@gmail.com>
2019-03-01 22:02:50 +0900

    第６回目の課題を提出

---
+++ 31_yamamura(kadai06)/ColLesson01/src/ColLesson1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ColLesson1 {
+    public static void main(String[] args) {
+
+        Word word1 = new Word(
+                "Apple", "りんご");
+
+
+
+
+
+        String[] word = new String[10];
+
+    }
+}
---
+++ 31_yamamura(kadai06)/ColLesson01/src/Word.java
@@ -0,0 +1,9 @@
+public class Word {
+    public String word;
+    public String meaning;
+
+    public Word(String input_word, String input_meaning) {
+        word = input_word;
+        meaning = input_meaning;
+    }
+}
