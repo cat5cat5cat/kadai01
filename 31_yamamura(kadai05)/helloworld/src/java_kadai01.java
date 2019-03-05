143657466d6adbd93d1fc6f9e5fe09ce46c46f95
cat5cat5cat <cat0melody@gmail.com>
2019-03-05 23:29:22 +0900

    第５回の課題の再提出（２回目：ファイル差し替え）です。

--- 31_yamamura(kadai05)/helloworld/src/java_kadai01.java
+++ 31_yamamura(kadai05)/helloworld/src/java_kadai01.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
-public class java_kadai01 {
-
+public class  java_kadai01{
+    // ランダムに質問を出す(重複して質問が出ないVer1)
     public static void main(String[] args) {
         // 質問を格納する配列の宣言・初期化
         String[] questions = {"あなたは猫は好きですか？",
@@ -11,6 +13,10 @@ public class java_kadai01 {
                 "あなたは旅行は好き？"};
         // スキャナー変数の宣言
         Scanner scanner = new Scanner(System.in);
+
+        // ランダム変数の宣言
+        Random random = new Random();
+
         // 値の合計値を集計する変数の宣言
         int sum = 0;
         // 答えを答えた数を集計する変数の宣言
@@ -18,9 +24,24 @@ public class java_kadai01 {
         // 入力した内容を格納する変数の宣言
         int input_num = 0;
 
+        // 乱数を格納する変数の宣言
+        int rand_num = 0;
+
+        // 今まで答えた質問を格納しておく配列を宣言
+        // あとで配列の値がチェックできるようにIntegerという型にします
+        Integer[] answered = new Integer[questions.length];
 
         for (int i = 0; i < questions.length; i++) {
-            System.out.println(questions[i]);
+            rand_num = random.nextInt(questions.length);
+            // 配列の中に値が入っているかは↓の文法でチェックします
+            if (Arrays.asList(answered).contains(rand_num)){
+                // もしすでに入ってたらやりなおしで continue文を呼び出す！
+                i--;
+                continue;
+            }
+            answered[answer_count] = rand_num;
+
+            System.out.println(questions[rand_num]);
             System.out.println("1: あてはまらない 2: あまりあてはらまらない 3: まあまああてはまる 4: あてはまる");
 
             input_num = scanner.nextInt();
@@ -46,4 +67,3 @@ public class java_kadai01 {
         }
     }
 }
-
