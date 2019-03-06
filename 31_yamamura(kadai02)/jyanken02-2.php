de151a8196a31d366150b8628628de4f0259ac24
cat5cat5cat <cat0melody@gmail.com>
2019-03-06 23:23:28 +0900

    第二回の課題ファイル消してしまったため再度アップ

--- 31_yamamura(kadai02)/jyanken02-2.php
+++ 31_yamamura(kadai02)/jyanken02-2.php
@@ -1,3 +1,44 @@
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!-- <?php -->
+	//（３）　
+	// $my_hand = array('グー', 'チョキ', 'パー');
+	// $my_hand_jp = array('グー', 'チョキ', 'パー');
+	// $aite_hand_jp = array('グー', 'チョキ', 'パー');
+	//（４）　
+	// $battle = $my_hand[$hand] - $aite_hand;
+	//（５）　
+	// switch ($battle) {
+		// case 'グー' :
+		// case 'チョキ' :
+			// $result = "あなたの勝ちです。";
+			// break;
+		// case 'パー'  :
+			// $result = "引き分けです。";
+			// break;
+		// default :
+			// $result = "あなたの負けです……";
+    // }
+    // ?>
+	<h1>janken.php(結果)</h1>
+	<div class="main">
+		<h2>あなた : $my_hand_out 相手 : $aite_hand_out </h2>
+		<h2>結果は …… $result </h2>
+		<p>
+			<a href="janken02-2.php">もう一回勝負！</a>
+		</p>
+	</div>  
+</body>
+</html> -->
+
 
 <!DOCTYPE html>
 <html lang="en">
