10f806ad87b9b079d3c3a1a688b2c702d8ff1fd3
cat5cat5cat <cat0melody@gmail.com>
2019-03-09 23:40:50 +0900

    第二回課題です

--- 31_yamamura(kadai02)/jyanken02-2.php
+++ 31_yamamura(kadai02)/jyanken02-2.php
@@ -1,45 +1,3 @@
-<!-- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<!-- <?php
-	//（３）　
-	// $my_hand = array('グー', 'チョキ', 'パー');
-	// $my_hand_jp = array('グー', 'チョキ', 'パー');
-	// $aite_hand_jp = array('グー', 'チョキ', 'パー');
-	//（４）　
-	// $battle = $my_hand[$hand] - $aite_hand;
-	//（５）　
-	// switch ($battle) {
-		// case 'グー' :
-		// case 'チョキ' :
-			// $result = "あなたの勝ちです。";
-			// break;
-		// case 'パー'  :
-			// $result = "引き分けです。";
-			// break;
-		// default :
-			// $result = "あなたの負けです……";
-    // }
-    // ?>
-	<h1>janken.php(結果)</h1>
-	<div class="main">
-		<h2>あなた : $my_hand_out 相手 : $aite_hand_out </h2>
-		<h2>結果は …… $result </h2>
-		<p>
-			<a href="janken02-2.php">もう一回勝負！</a>
-		</p>
-	</div>  
-</body>
-</html> -->
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -86,7 +44,8 @@
 	$my_hand_jp = array('グー', 'チョキ', 'パー');
 	$aite_hand_jp = array('グー', 'チョキ', 'パー');
 	//（４）　
-	$battle = $my_hand[$hand] - $aite_hand;
+	// $battle = $my_hand[$hand] - $aite_hand;
+	// echo $battle
 	//（５）　
 	switch ($battle) {
 		case 'グー' :
@@ -101,7 +60,7 @@
     }
     ?>
 	<h1>janken.php(結果)</h1>
-	<h1><?php echo $hands; ?></h1>
+	<!-- <h1><?php echo $hand; ?></h1> -->
 	<div class="main">
 		<h2>あなた :<?echo $my_hand_out;?> 相手 : <? echo $aite_hand_out;?> </h2>
 		<h2>結果は …… <? echo $result;?> </h2>
